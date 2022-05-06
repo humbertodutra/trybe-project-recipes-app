812b2ecf40127dc9d8d26cca5f2f22548c41beb3
Christian Mendes Pereira <christianp3m@gmail.com>
2022-05-06 14:26:53 -0300

    Done, pendent tests

---
+++ src/Components/ProfileComponent.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+function ProfileComponent() {
+  const history = useHistory();
+  const user = localStorage.getItem('user');
+  const userObj = JSON.parse(user);
+  console.log(userObj.email);
+  const storage = localStorage;
+  return (
+    <main>
+
+      <p data-testid="profile-email">{userObj.email}</p>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ () => { history.push({ pathname: '/done-recipes' }); } }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ () => { history.push({ pathname: '/favorite-recipes' }); } }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ () => {
+          storage.clear();
+          history.push({ pathname: '/' });
+        } }
+      >
+        Logout
+      </button>
+
+    </main>
+  );
+}
+
+export default ProfileComponent;
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../Components/Header';
 import Footer from '../../Components/Footer/Footer';
+import ProfileComponenet from '../../Components/ProfileComponent';
 
 export default function Profile(props) {
   return (
     <div>
       <Header { ...props } title="Profile" dontShowSearchIcon />
+      <ProfileComponenet />
       <Footer />
     </div>
   );
