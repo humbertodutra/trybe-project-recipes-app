4a90e84e2386c71b487b3adc9c71cc8108a2bb64
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-05-06 21:51:19 -0300

    testes da tela de perfil

--- src/ExploreDrinksIngredients.test.js
+++ src/ExploreDrinksIngredients.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import ExploreDrinksIngredients from './Pages/Explore/ExploreDrinksIngredients';
 
@@ -10,5 +11,13 @@ describe('Testes da tela Explore Drinks Ingredients', () => {
       name: /light rum/i });
     expect(btnIngredient).toBeInTheDocument();
     expect(btnIngredient).toHaveAttribute('data-testid', '0-ingredient-card');
+  });
+  it('Verifica se ao clicar no card, é redirecionado corretamente', async () => {
+    const { history } = renderWithRouter(<ExploreDrinksIngredients />);
+    const btnIngredient = await screen.findByRole('button', { name: /light rum/i });
+    userEvent.click(btnIngredient);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    // não está indo pra rota "/foods"
   });
 });
--- src/ExploreFoodsIngredients.test.js
+++ src/ExploreFoodsIngredients.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import ExploreFoodsIngredients from './Pages/Explore/ExploreFoodsIngredients';
 
@@ -9,5 +10,13 @@ describe('Testes da tela Explore Foods Ingredients', () => {
     const button = await screen.findByRole('button', { name: /chicken/i });
     expect(button).toBeInTheDocument();
     expect(button).toHaveAttribute('data-testid', '0-ingredient-card');
+  });
+  it('Verifica se ao clicar no card, é redirecionado corretamente', async () => {
+    const { history } = renderWithRouter(<ExploreFoodsIngredients />);
+    const button = await screen.findByRole('button', { name: /chicken/i });
+    userEvent.click(button);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    // não está indo pra rota "/drinks"
   });
 });
