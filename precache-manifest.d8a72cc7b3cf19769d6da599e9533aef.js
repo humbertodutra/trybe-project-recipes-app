b19e2a8c09d240d9a0b35b688f81601423d116f6
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-04 13:46:44 -0300

    Updates

---
+++ precache-manifest.d8a72cc7b3cf19769d6da599e9533aef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0ea27686a31efabfe8a",
+    "url": "/trybe-project-recipes-app/static/css/main.6b86054f.chunk.css"
+  },
+  {
+    "revision": "c0ea27686a31efabfe8a",
+    "url": "/trybe-project-recipes-app/static/js/main.c0ea2768.chunk.js"
+  },
+  {
+    "revision": "20a73e178f0d3c979391",
+    "url": "/trybe-project-recipes-app/static/css/1.0b1691e1.chunk.css"
+  },
+  {
+    "revision": "20a73e178f0d3c979391",
+    "url": "/trybe-project-recipes-app/static/js/1.20a73e17.chunk.js"
+  },
+  {
+    "revision": "a1dd4ea77f88fdb2aa67",
+    "url": "/trybe-project-recipes-app/static/js/runtime~main.a1dd4ea7.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/trybe-project-recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/trybe-project-recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/trybe-project-recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/trybe-project-recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/trybe-project-recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/trybe-project-recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/trybe-project-recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/trybe-project-recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "afd3c7b7844f4565ec8edc3f2ffeea56",
+    "url": "/trybe-project-recipes-app/index.html"
+  }
+];
